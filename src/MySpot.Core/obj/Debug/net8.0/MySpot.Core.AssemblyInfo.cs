fd359705ed73665074794a6e5c5a24dac8d783f1
Yehor Pliasov <fenix@yehors-macbook-air.home>
2024-11-05 18:33:51 +0100

    db created

--- src/MySpot.Core/obj/Debug/net8.0/MySpot.Core.AssemblyInfo.cs
+++ src/MySpot.Core/obj/Debug/net8.0/MySpot.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySpot.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4f3511fa8eaf81a881a2ad8da21d87c0231cbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6ff12cda9384321c80d9ab9221198a2a27243d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySpot.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySpot.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
